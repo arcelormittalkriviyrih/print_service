058891903d48720c7b8f78d4d9e2d237743d51db
Oleksandr Chekmez <chekmez@nikama.com>
2016-05-11 12:59:20 +0300

    #161 Сделать консольную версию сервиса печати

---
+++ PrintWindowsConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//using System.Threading. Tasks;
+
+namespace PrintWindowsService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintJobs pJobs = new PrintJobs();
+            pJobs.StartJob();
+            Console.WriteLine("Press Esc to exit");
+            do
+            {
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+            pJobs.StopJob();
+        }
+    }
+}
