b0665effeb7ff5294bafccabd03332abf4b4bbef
Oleksandr Chekmez <chekmez@nikama.com>
2016-06-13 14:57:17 +0300

    #116 add script to install print service

--- PrintLabelService/install.bat
+++ PrintLabelService/install.bat
@@ -1,2 +1,8 @@
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe "C:\Nikama\print_service\Service\PrintLabelService.exe"
-pause
+rem uninstall existing service
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe /u "C:\Nikama\print_service\PrintLabelService.exe"
+rem copy new version
+xcopy %WORKSPACE%\PrintLabelService\bin\Debug\*.* C:\Nikama\print_service /Y
+rem install existing service
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe /username=europe\krr-svc-palbp-auto /password=%password% /unattended "C:\Nikama\print_service\PrintLabelService.exe"
+rem start service
+net start "ArcelorMittal.PrintService"
