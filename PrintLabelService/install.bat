d19b3ac91515607fb5568ae86c53d35a84d1376c
Oleksandr Chekmez <chekmez@nikama.com>
2016-06-13 15:38:24 +0300

    #116 add credentials for install script

--- PrintLabelService/install.bat
+++ PrintLabelService/install.bat
@@ -3,6 +3,8 @@ C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe /u "C:\Nikama\prin
 rem copy new version
 xcopy %WORKSPACE%\PrintLabelService\bin\Debug\*.* C:\Nikama\print_service /Y
 rem install existing service
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe /username=europe\krr-svc-palbp-auto /password=%password% /unattended "C:\Nikama\print_service\PrintLabelService.exe"
+echo off
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe /username=%PRINT_USER% /password=%PRINT_PASS% /unattended "C:\Nikama\print_service\PrintLabelService.exe"
+echo on
 rem start service
 net start "ArcelorMittal.PrintService"
