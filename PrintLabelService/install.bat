0ba8964c2049e9a52febb357243c56c7e4e7fa07
Oleksandr Chekmez <chekmez@nikama.com>
2016-06-13 15:41:32 +0300

    #116 change config to Release

--- PrintLabelService/install.bat
+++ PrintLabelService/install.bat
@@ -1,7 +1,7 @@
 rem uninstall existing service
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe /u "C:\Nikama\print_service\PrintLabelService.exe"
 rem copy new version
-xcopy %WORKSPACE%\PrintLabelService\bin\Debug\*.* C:\Nikama\print_service /Y
+xcopy %WORKSPACE%\PrintLabelService\bin\Release\*.* C:\Nikama\print_service /Y
 rem install existing service
 echo off
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe /username=%PRINT_USER% /password=%PRINT_PASS% /unattended "C:\Nikama\print_service\PrintLabelService.exe"
