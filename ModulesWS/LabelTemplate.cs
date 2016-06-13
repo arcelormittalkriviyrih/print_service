8c82b424de6346c5a1e8a7c527854918452d8fb3
Oleksandr Chekmez <chekmez@nikama.com>
2016-06-13 07:41:41 +0300

    #98 fixes for Excel template fill

--- ModulesWS/LabelTemplate.cs
+++ ModulesWS/LabelTemplate.cs
@@ -24,6 +24,34 @@ namespace PrintWindowsService
             worksheetPartParams = (WorksheetPart)(workbookpart.GetPartById(worksheetParams.Id));
         }
 
+        private static int InsertSharedStringItem(string text, SharedStringTablePart shareStringPart)
+        {
+            // If the part does not contain a SharedStringTable, create one.
+            if (shareStringPart.SharedStringTable == null)
+            {
+                shareStringPart.SharedStringTable = new SharedStringTable();
+            }
+
+            int i = 0;
+
+            // Iterate through all the items in the SharedStringTable. If the text already exists, return its index.
+            foreach (SharedStringItem item in shareStringPart.SharedStringTable.Elements<SharedStringItem>())
+            {
+                if (item.InnerText == text)
+                {
+                    return i;
+                }
+
+                i++;
+            }
+
+            // The text does not exist in the part. Create the SharedStringItem and return its index.
+            shareStringPart.SharedStringTable.AppendChild(new SharedStringItem(new DocumentFormat.OpenXml.Spreadsheet.Text(text)));
+            shareStringPart.SharedStringTable.Save();
+
+            return i;
+        }
+
         private Cell CheckEmptyCell(Row currentRow, Cell afterCell, string columnCell)
         {
             Cell returnCell = currentRow.Elements<Cell>().Where(c => c.CellReference.Value == columnCell + currentRow.RowIndex).FirstOrDefault();
@@ -97,6 +125,16 @@ namespace PrintWindowsService
         /// </summary>
         public void FillParamValues(PrintJobProps aJobProps)
         {
+            SharedStringTablePart shareStringPart;
+            if (spreadSheet.WorkbookPart.GetPartsOfType<SharedStringTablePart>().Count() > 0)
+            {
+                shareStringPart = spreadSheet.WorkbookPart.GetPartsOfType<SharedStringTablePart>().First();
+            }
+            else
+            {
+                shareStringPart = spreadSheet.WorkbookPart.AddNewPart<SharedStringTablePart>();
+            }
+
             foreach (Row rowParam in worksheetPartParams.Worksheet.GetFirstChild<SheetData>().Elements<Row>())
             {
                 Cell refCellA = rowParam.Elements<Cell>().Where(c => c.CellReference.Value == "A" + rowParam.RowIndex).FirstOrDefault();
@@ -107,8 +145,11 @@ namespace PrintWindowsService
                 else
                 {
                     Cell refCellB = CheckEmptyCell(rowParam, refCellA, "B");
+                    worksheetPartParams.Worksheet.Save();
                     Cell refCellC = CheckEmptyCell(rowParam, refCellB, "C");
+                    worksheetPartParams.Worksheet.Save();
                     Cell refCellD = CheckEmptyCell(rowParam, refCellC, "D");
+                    worksheetPartParams.Worksheet.Save();
 
                     /*if (rowParam.RowIndex == 1)
                     {
@@ -118,13 +159,13 @@ namespace PrintWindowsService
                     }
                     else
                     {*/
-                        //these rows for other params
-                        refCellD.CellValue = new CellValue(aJobProps.getLabelParameter(GetCellValue(refCellA), GetCellValue(refCellB)));
-                        refCellD.DataType = new EnumValue<CellValues>(CellValues.String);
+                    //these rows for other params
+                    int index = InsertSharedStringItem(aJobProps.getLabelParameter(GetCellValue(refCellA), GetCellValue(refCellB)), shareStringPart);
+                    refCellD.CellValue = new CellValue(index.ToString());
+                    refCellD.DataType = new EnumValue<CellValues>(CellValues.SharedString);
                     //}
                 }
             }
-            worksheetPartParams.Worksheet.Save();
             RecalcRefCellValues();
             workbookpart.Workbook.Save();
             spreadSheet.Close();
