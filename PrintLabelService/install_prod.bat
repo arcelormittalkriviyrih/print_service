3696fa574ad564ab2ead47c479e6a0cfd8cba014
Oleksandr Chekmez <chekmez@nikama.com>
2016-09-02 14:40:31 +0300

    change  folder path

--- PrintLabelService/install_prod.bat
+++ PrintLabelService/install_prod.bat
@@ -1,7 +1,7 @@
 rem uninstall existing service
-sc.exe \\krr-app-palbp01 Delete "ArcelorMittal.PrintService_test"
+sc.exe \\krr-app-palbp01 Delete "ArcelorMittal.PrintService"
 rem copy new version
-xcopy %WORKSPACE%\PrintLabelService\bin\Production\*.* \\krr-app-palbp01\Nikama\print_service_test /Y
+xcopy %WORKSPACE%\PrintLabelService\bin\Production\*.* \\krr-app-palbp01\Nikama\print_service /Y
 rem install existing service
 rem echo off
-sc.exe \\krr-app-palbp01 Create "ArcelorMittal.PrintService_test" binPath="C:\Nikama\print_service_test\PrintLabelService.exe" start=demand obj=%PRINT_USER% password=%PRINT_PASS%
+sc.exe \\krr-app-palbp01 Create "ArcelorMittal.PrintService" binPath="C:\Nikama\print_service\PrintLabelService.exe" start=auto obj=%PRINT_USER% password=%PRINT_PASS%
