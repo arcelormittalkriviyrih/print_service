9637eace11bd87e51766679a7ac609e67ab8804b
Sergey Arhipov <Sergey.Arhipov@arcelormittal.com>
2017-02-16 14:35:29 +0200

    first run with administrator privileges

--- PrintLabelService/install.bat
+++ PrintLabelService/install.bat
@@ -4,8 +4,12 @@ rem copy new version
 xcopy %WORKSPACE%\PrintLabelService\bin\Release\*.* C:\Nikama\print_service /Y
 rem install existing service
 echo off
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe /username=%PRINT_USER% /password=%PRINT_PASS% /unattended "C:\Nikama\print_service\PrintLabelService.exe"
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe /username=%ADMIN_USER% /password=%ADMIN_PASS% /unattended "C:\Nikama\print_service\PrintLabelService.exe"
 echo on
+rem first run with administrator privileges
+net start "ArcelorMittal.PrintService"
+net stop "ArcelorMittal.PrintService"
+sc.exe config "ArcelorMittal.PrintService" obj=%PRINT_USER% password=%PRINT_PASS%
 rem configure delayed service
 sc.exe config "ArcelorMittal.PrintService" start=delayed-auto
 rem net stop "ArcelorMittal.PrintService"
--- PrintLabelService/install_prod.bat
+++ PrintLabelService/install_prod.bat
@@ -5,5 +5,9 @@ rem copy new version
 xcopy %WORKSPACE%\PrintLabelService\bin\Production\*.* \\krr-app-palbp01\Nikama\print_service /Y
 rem install existing service
 rem echo off
-sc.exe \\krr-app-palbp01 Create "ArcelorMittal.PrintService" binPath="C:\Nikama\print_service\PrintLabelService.exe" start=delayed-auto obj=%PRINT_USER% password=%PRINT_PASS%
+rem first run with administrator privileges
+sc.exe \\krr-app-palbp01 Create "ArcelorMittal.PrintService" binPath="C:\Nikama\print_service\PrintLabelService.exe" start=delayed-auto obj=%ADMIN_USER% password=%ADMIN_PASS%
+sc.exe \\krr-app-palbp01 Start "ArcelorMittal.PrintService"
+sc.exe \\krr-app-palbp01 Stop "ArcelorMittal.PrintService"
+sc.exe config "ArcelorMittal.PrintService" obj=%PRINT_USER% password=%PRINT_PASS%
 sc.exe \\krr-app-palbp01 Start "ArcelorMittal.PrintService"
