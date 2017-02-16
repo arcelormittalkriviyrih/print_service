dba869f147010aae63fc9b8259edbf6a329eec50
Sergey Arhipov <Sergey.Arhipov@arcelormittal.com>
2017-02-16 14:46:49 +0200

    fix a typo name server

--- PrintLabelService/install_prod.bat
+++ PrintLabelService/install_prod.bat
@@ -9,5 +9,5 @@ rem first run with administrator privileges
 sc.exe \\krr-app-palbp01 Create "ArcelorMittal.PrintService" binPath="C:\Nikama\print_service\PrintLabelService.exe" start=delayed-auto obj=%ADMIN_USER% password=%ADMIN_PASS%
 sc.exe \\krr-app-palbp01 Start "ArcelorMittal.PrintService"
 sc.exe \\krr-app-palbp01 Stop "ArcelorMittal.PrintService"
-sc.exe config "ArcelorMittal.PrintService" obj=%PRINT_USER% password=%PRINT_PASS%
+sc.exe \\krr-app-palbp01 config "ArcelorMittal.PrintService" obj=%PRINT_USER% password=%PRINT_PASS%
 sc.exe \\krr-app-palbp01 Start "ArcelorMittal.PrintService"
